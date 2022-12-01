aa109f706dfdf5f7025ec9f4724c833020a73faf
sabitskar <56510622+sabitskar@users.noreply.github.com>
2022-12-01 12:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 st.write("Here's my first attempt to create a project:")
-st.write("Division of 2 given numbers")
+st.header("Division of 2 given numbers")
 st.header("User input parameter")
 x = st.number_input('1st number')
 y = st.number_input('2nd Number')
